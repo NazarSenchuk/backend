6fde472aff1d3d91f5c159ebd1356fa38452672b
NazarSenchuk <113719257+NazarSenchuk@users.noreply.github.com>
2024-11-03 13:57:10 +0000

    Update db.py

--- app/db.py
+++ app/db.py
@@ -3,7 +3,7 @@ import os
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:gYwOKJZaR0do8TUUgPS9@pg:5432/postgres" 
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:senchuknazar6@database-1.cfsws6qa2j6z.us-east-1.rds.amazonaws.com:5432/postgres" 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
